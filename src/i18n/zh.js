02fe9f1c9a658415894f9344a8f66006ed236a97
yaoxin <871966163@qq.com>
2020-06-30 17:59:24 +0800

    添加拖拽上传文件

--- src/i18n/zh.js
+++ src/i18n/zh.js
@@ -12,7 +12,11 @@ const zh = {
       { num: "2", text: "预览数据" },
       { num: "3", text: "工作表设置" }
     ],
-    cardHeaderTitle: "请选择要上传的文件"
+    cardHeaderTitle: "请选择要上传的文件",
+    uploadLinkText: "点击上传文件",
+    uploadDragText: "或者拖拽上传",
+    uploadFileIntro:
+      "支持Excel和CSV文件（单个Excel最大100M，CSV最大200M）, 最多5个文件批量上传，默认识别第一个sheet文件"
   }
 };
 
--- src/views/data_source/AddDataSource.vue
+++ src/views/data_source/AddDataSource.vue
@@ -19,8 +19,11 @@
           md="4"
           lg="3"
           xl="2"
-          col-class="text-center"
-          :style="item.active ? null : 'opacity: 0.5'"
+          :class="
+            item.active
+              ? 'text-center'
+              : 'text-center nly-data-analysis-opacity-1'
+          "
           v-for="(item, index) in step"
           :key="index"
         >
@@ -38,12 +41,36 @@
         </nly-card-header>
         <nly-card-body card-body-class="add-data-source-card-upload-file">
           <nly-row
-            row-class="add-data-source-card-upload-tips"
+            :row-class="
+              drapFileHover
+                ? 'add-data-source-card-upload-tips nly-data-analysis-backgroup-gray'
+                : 'add-data-source-card-upload-tips'
+            "
             align-h="center"
             align-v="center"
+            id="upload-file"
           >
-            <nly-col>
-              <nly-form-input type="file" />
+            <nly-col col-class="text-center text-sm">
+              <nly-row row-class="nly-data-analysis-padding-1">
+                <nly-col>
+                  <span>
+                    <nly-link>{{
+                      this.$lang.addDataSource.uploadLinkText
+                    }}</nly-link>
+                    {{ this.$lang.addDataSource.uploadDragText }}
+                  </span>
+                </nly-col>
+              </nly-row>
+              <nly-row align-h="center">
+                <nly-col
+                  xs="10"
+                  md="6"
+                  lg="4"
+                  col-class="nly-data-analysis-opacity-1"
+                >
+                  {{ this.$lang.addDataSource.uploadFileIntro }}
+                </nly-col>
+              </nly-row>
             </nly-col>
           </nly-row>
         </nly-card-body>
@@ -59,7 +86,10 @@ export default {
   components: { "nly-data-analysis-data-header": NlyDataAnalysisDataHeader },
   data() {
     return {
-      step: this.$lang.addDataSource.step
+      step: this.$lang.addDataSource.step,
+      drapFileHover: false, //文件拖拖动到区域的hover效果
+      imgArr: [],
+      fileData: []
     };
   },
   created() {
@@ -70,6 +100,77 @@ export default {
         this.step[index]["active"] = false;
       }
     });
+  },
+  mounted: function() {
+    const dropbox = document.getElementById("upload-file");
+    dropbox.addEventListener("drop", this.drapFileConfirm);
+    dropbox.addEventListener("dragleave", this.drapFileLeave, true);
+    dropbox.addEventListener("dragenter", this.drapFileEnter);
+    dropbox.addEventListener("dragover", this.drapFileOver);
+  },
+  beforeDestroy() {
+    const dropbox = document.getElementById("upload-file");
+    dropbox.removeEventListener("drop", this.drapFileEnter, true);
+    dropbox.removeEventListener("drop", this.drapFileEnter, true);
+    dropbox.removeEventListener("drop", this.drapFileEnter, true);
+  },
+  methods: {
+    getTargetNode(ele, target) {
+      //ele是内部元素，target是你想找到的包裹元素
+      if (!ele || ele === document) return false;
+      return ele === target ? true : this.getTargetNode(ele.parentNode, target);
+    },
+    drapFileConfirm(e) {
+      console.log(1);
+      this.drapFileHover = false;
+      e.stopPropagation();
+      e.preventDefault(); //必填字段
+      let fileData = e.dataTransfer.files;
+      console.log(fileData);
+      this.uploadFile(fileData);
+    },
+    drapFileLeave(e) {
+      const target = document.getElementById("upload-file");
+      if (!this.getTargetNode(e.target, target)) {
+        this.drapFileHover = false;
+      }
+      e.stopPropagation();
+      e.preventDefault();
+    },
+    drapFileEnter(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      this.drapFileHover = true;
+    },
+    drapFileOver(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      this.drapFileHover = true;
+    },
+    uploadFile: function(file) {
+      //渲染上传文件
+      for (let i = 0; i !== file.length; i++) {
+        let fileJson = {
+          Url: "",
+          progressStatl: 0,
+          fileText: ""
+        };
+        let video_type =
+          file[i].type == "video/mp4" || file[i].type == "video/ogg";
+        if (file[i].type.indexOf("image") === 0) {
+          //如果是图片
+          let fileurl = window.URL.createObjectURL(file[i]); //创建一个url连接,供src属性引用
+          fileJson.Url = fileurl;
+        } else if (video_type) {
+          // eslint-disable-next-line no-unused-vars
+          let fileurl = window.URL.createObjectURL(file[i]); //创建一个url连接,供src属性引用
+        } else {
+          alert("不支持此类型文件");
+        }
+        fileJson.fileText = file[i].name;
+        this.fileData.push(fileJson);
+      }
+    }
   }
 };
 </script>
